8a8771a6409519c4bfecf597285766d908cc8ea8
talanc <talanc@users.noreply.github.com>
2022-02-06 14:54:57 +1100

    hit

--- RTI1W/Hittable.cs
+++ RTI1W/Hittable.cs
@@ -19,3 +19,78 @@ public struct HitRecord
         Normal = FrontFace ? outwardNormal : -outwardNormal;
     }
 }
+
+public class HittableList : Hittable
+{
+    public List<Hittable> List = new List<Hittable>();
+
+    public override HitRecord? Hit(Ray r, double tMin, double tMax)
+    {
+        HitRecord? rec = null;
+        var closestSoFar = tMax;
+
+        foreach (var obj in List)
+        {
+            var objRec = obj.Hit(r, tMin, closestSoFar);
+            if (objRec.HasValue)
+            {
+                rec = objRec;
+                closestSoFar = objRec.Value.T;
+            }
+        }
+
+        return rec;
+    }
+}
+
+public class Sphere : Hittable
+{
+    public Vec3 Center;
+    public double Radius;
+    public Material Material;
+
+    public Sphere(Vec3 center, double radius, Material material)
+    {
+        Center = center;
+        Radius = radius;
+        Material = material;
+    }
+
+    public override HitRecord? Hit(Ray r, double tMin, double tMax)
+    {
+        var oc = r.Origin - Center;
+        var a = r.Direction.LengthSquared;
+        var halfB = Dot(oc, r.Direction);
+        var c = oc.LengthSquared - Radius * Radius;
+
+        var discriminant = halfB * halfB - a * c;
+        if (discriminant < 0)
+        {
+            return null;
+        }
+        var sqrtD = Sqrt(discriminant);
+
+        var root = (-halfB - sqrtD) / a;
+
+        if (root < tMin || tMax < root)
+        {
+            root = (-halfB + sqrtD) / a;
+            if (root < tMin || tMax < root)
+            {
+                return null;
+            }
+        }
+
+        var t = root;
+        var p = r.At(t);
+        var outwardNormal = (p - Center) / Radius;
+        var rec = new HitRecord()
+        {
+            P = p,
+            T = t,
+            Material = Material,
+        };
+        rec.SetFaceNormal(r, outwardNormal);
+        return rec;
+    }
+}
