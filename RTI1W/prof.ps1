61689333e5f702a01d451b447210100b3c3b378f
talanc <talanc@users.noreply.github.com>
2026-01-24 21:21:36 +1100

    prof.ps1

--- RTI1W/prof.ps1
+++ RTI1W/prof.ps1
@@ -1,6 +1,6 @@
 dotnet build -c Release
 if ($LASTEXITCODE -eq 0) {
     for ($i = 1; $i -le 5; $i++) {
-        (Measure-Command { .\bin\Release\net10.0\RTI1W.exe --width 450 --height 300 }).TotalSeconds
+        (Measure-Command { .\bin\Release\net10.0\RTI1W.exe --threads 1 --seed 10 }).TotalSeconds
     }
 }
