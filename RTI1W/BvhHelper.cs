40c5b1a7a651d23e104b06e8bb600a4e74a5464e
Thomas Copeland <talan.copeland@gmail.com>
2026-02-06 23:01:58 +1100

    improved bvh with sah (33% faster)

--- RTI1W/BvhHelper.cs
+++ RTI1W/BvhHelper.cs
@@ -1,70 +1,175 @@
 ﻿namespace RTI1W;
 
-public class BvhHelper
+public static class BvhHelper
 {
+    // See https://pbr-book.org/4ed/Primitives_and_Intersection_Acceleration/Bounding_Volume_Hierarchies
+
     public static BvhHittable CreateBvh(List<Hittable> hittables)
     {
         ArgumentOutOfRangeException.ThrowIfLessThan(hittables.Count, 2, nameof(hittables));
 
-        var hittableBoxArr = new NodeInfo[hittables.Count];
-        for (var i = 0; i < hittableBoxArr.Length; i++)
+        var nodes = new NodeInfo[hittables.Count];
+        for (var i = 0; i < hittables.Count; i++)
         {
-            var hittable = hittables[i];
-            var bounds = hittable.GetBoundingBox();
-            hittableBoxArr[i] = new NodeInfo(hittable, bounds, bounds.Middle());
+            var h = hittables[i];
+            nodes[i] = new NodeInfo(h, h.GetBoundingBox(), h.GetBoundingBox().Middle());
         }
 
-        var span = new Span<NodeInfo>(hittableBoxArr);
-        return (BvhHittable)CreateBvh(span);
+        return (BvhHittable)CreateBvh(nodes, 0);
     }
 
     private record class NodeInfo(Hittable Hittable, Box3 Bounds, Vector3 Middle);
 
-    private static Hittable CreateBvh(Span<NodeInfo> span)
+    private static Hittable CreateBvh(Span<NodeInfo> nodes, int depth)
     {
-        if (span.Length == 0)
+        if (nodes.Length == 1)
         {
-            throw new ArgumentException("span is empty", nameof(span));
+            return nodes[0].Hittable;
         }
 
-        var bounds = span[0].Bounds;
-        for (var i = 1; i < span.Length; i++)
+        if (nodes.Length <= 4 || depth > 32)
         {
-            bounds = Box3.Union(bounds, span[i].Bounds);
+            return CreateHittableList(nodes);
         }
 
-        if (span.Length == 1)
+        var bounds = nodes[0].Bounds;
+        for (var i = 1; i < nodes.Length; i++)
         {
-            return span[0].Hittable;
+            bounds = Box3.Union(bounds, nodes[i].Bounds);
         }
 
-        if (span.Length == 2)
+        var noSplitCost = nodes.Length;
+        var (cost, axis, pos) = FindBestSplit(nodes, bounds);
+        if (cost >= noSplitCost)
         {
-            return new BvhHittable(bounds, span[0].Hittable, span[1].Hittable);
+            return CreateHittableList(nodes);
         }
 
-        // TODO i think we can use a better partitioning algorithm
-        // see https://pbr-book.org/4ed/Primitives_and_Intersection_Acceleration/Bounding_Volume_Hierarchies
+        nodes.Sort((a, b) => a.Middle[axis].CompareTo(b.Middle[axis]));
 
+        var p = 1;
+        while (p < nodes.Length && nodes[p].Middle[axis] < pos)
+        {
+            p++;
+        }
+
+        var leftNodes = nodes.Slice(0, p);
+        var rightNodes = nodes.Slice(p);
+
+        var left = CreateBvh(leftNodes, depth + 1);
+        var right = CreateBvh(rightNodes, depth + 1);
+
+        var sah = new BvhHittable(bounds, left, right);
+
+        return sah;
+    }
+
+    private static HittableList CreateHittableList(Span<NodeInfo> nodes)
+    {
+        var list = new HittableList();
+        foreach (var node in nodes)
+        {
+            list.Add(node.Hittable);
+        }
+        return list;
+    }
+
+    private const float TraverseCost = 0.5f;
+    private const float IntersectCost = 1.0f;
+
+    private static (float Cost, int Axis, float Pos) FindBestSplit(Span<NodeInfo> nodes, Box3 bounds)
+    {
+        var bestCost = float.PositiveInfinity;
+        var bestAxis = 0;
+        var bestPos = 0.0f;
+
+        Span<float> splitPositions = stackalloc float[16];
+
+        for (var axis = 0; axis < 3; axis++)
+        {
+            if (!SetCandidatePositions(splitPositions, nodes, axis))
+            {
+                continue;
+            }
+
+            foreach (var pos in splitPositions)
+            {
+                var leftBounds = new Box3();
+                var rightBounds = new Box3();
+                var leftCount = 0;
+                var rightCount = 0;
+
+                foreach (var node in nodes)
+                {
+                    if (node.Middle[axis] < pos)
+                    {
+                        leftBounds = Box3.Union(leftBounds, node.Bounds);
+                        leftCount++;
+                    }
+                    else
+                    {
+                        rightBounds = Box3.Union(rightBounds, node.Bounds);
+                        rightCount++;
+                    }
+                }
+
+                var cost = EvaluateSah(bounds, leftBounds, rightBounds, leftCount, rightCount);
+
+                if (cost < bestCost)
+                {
+                    bestCost = cost;
+                    bestAxis = axis;
+                    bestPos = pos;
+                }
+            }
+        }
+
+        return (bestCost, bestAxis, bestPos);
+    }
+
+    private static float EvaluateSah(Box3 parentBounds, Box3 leftBounds, Box3 rightBounds, int leftCount, int rightCount)
+    {
+        var parentSA = CalculateSurfaceArea(parentBounds);
+        var leftSA = CalculateSurfaceArea(leftBounds);
+        var rightSA = CalculateSurfaceArea(rightBounds);
+
+        var cost = TraverseCost +
+            leftSA / parentSA * leftCount * IntersectCost +
+            rightSA / parentSA * rightCount * IntersectCost;
+
+        return cost;
+    }
+
+    private static float CalculateSurfaceArea(Box3 bounds)
+    {
         var size = bounds.Size();
-        if (size.X > size.Y && size.X > size.Z)
+        return 2 * (size.X * size.Y + size.Y * size.Z + size.Z * size.X);
+    }
+
+    // Returns false if we cannot provide candidate positions
+    private static bool SetCandidatePositions(Span<float> positions, Span<NodeInfo> nodes, int axis)
+    {
+        var min = float.MaxValue;
+        var max = float.MinValue;
+        foreach (var node in nodes)
         {
-            span.Sort((a, b) => (int)((a.Middle.X - b.Middle.X) * 1000));
-        }
-        else if (size.Y > size.Z)
-        {
-            span.Sort((a, b) => (int)((a.Middle.Y - b.Middle.Y) * 1000));
-        }
-        else
-        {
-            span.Sort((a, b) => (int)((a.Middle.Z - b.Middle.Z) * 1000));
+            min = Min(min, node.Middle[axis]);
+            max = Max(max, node.Middle[axis]);
         }
 
-        var size1 = span.Length / 2;
-        var size2 = span.Length - size1;
+        if (min > max || (max - min) < 1e-5f)
+        {
+            return false;
+        }
 
-        var partition1 = span.Slice(0, size1);
-        var partition2 = span.Slice(size1, size2);
-        return new BvhHittable(bounds, CreateBvh(partition1), CreateBvh(partition2));
+        var inc = (max - min) / (positions.Length + 1);
+
+        for (var i = 0; i < positions.Length; i++)
+        {
+            positions[i] = min + (i + 1) * inc;
+        }
+
+        return true;
     }
 }
+
