293f4e4a931b40ee33db7e120aaaa319d19756e8
Thomas Copeland <talan.copeland@gmail.com>
2026-02-10 20:16:16 +1100

    ten

--- RTI1W/prof.ps1
+++ RTI1W/prof.ps1
@@ -1,6 +1,6 @@
 dotnet build -c Release
 if ($LASTEXITCODE -eq 0) {
-    for ($i = 1; $i -le 5; $i++) {
+    for ($i = 1; $i -le 10; $i++) {
         (Measure-Command { .\bin\Release\net10.0\RTI1W.exe --threads 1 --seed 10 }).TotalSeconds
     }
 }
