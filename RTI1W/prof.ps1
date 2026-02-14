c56831bfbac3b2cabf3062e23b2c547b0f33eeb2
Thomas Copeland <talan.copeland@gmail.com>
2026-02-14 15:25:41 +1100

    prof.ps1

--- RTI1W/prof.ps1
+++ RTI1W/prof.ps1
@@ -1,6 +1,12 @@
 dotnet build -c Release
 if ($LASTEXITCODE -eq 0) {
+    Write-Output "BVH=Tree"
     for ($i = 1; $i -le 10; $i++) {
-        (Measure-Command { .\bin\Release\net10.0\RTI1W.exe --threads 1 --seed 10 }).TotalSeconds
+        (Measure-Command { .\bin\Release\net10.0\RTI1W.exe --threads 1 --seed 10 --bvh Tree }).TotalSeconds
+    }
+
+    Write-Output "BVH=Linear"
+    for ($i = 1; $i -le 10; $i++) {
+        (Measure-Command { .\bin\Release\net10.0\RTI1W.exe --threads 1 --seed 10 --bvh Linear }).TotalSeconds
     }
 }
